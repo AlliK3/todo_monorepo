b7f4a42dda28b656d9b86629f99bb83bd4b0b18e
Ali <alaliali@msu.edu>
2025-06-30 13:16:50 +0300

    Some updates

--- packages/todo_supabase/lib/main.dart
+++ packages/todo_supabase/lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:todo_supabase/home.dart';
 import 'package:todo_ui/task_dialog.dart';
 
 void main() async{
+  
+  await Supabase.initialize(
+  url: 'https://wgoocxhsoudslioecgok.supabase.col',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indnb29jeGhzb3Vkc2xpb2VjZ29rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEyNzE3MTcsImV4cCI6MjA2Njg0NzcxN30.PI9D3VTh6hiVwHmpvwgIuWDpk00FxLcW5W3FvIPSoIk',
+);
+
   runApp(const MyApp());
 }
 
---
+++ packages/todo_supabase/lib/todo_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo_ui/todo.dart';
+
+class TodoProvider extends ChangeNotifier {
+  final TodoList _todoList = const TodoList([]);
+  bool _isLoading = false;
+
+  Future<void> addTodo(String title) async {
+    _isLoading = true;
+    notifyListeners();
+    // Add to Supabase and update local state
+    _isLoading = false;
+    notifyListeners();
+  }
+}
